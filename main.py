d91ca62317c349316c51a871f764f6d9651b09f7
Siddapa <vishnuschalla@gmail.com>
2021-05-14 16:31:42 -0500

    Added Point Awarding

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import json
+import csv
 
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 bot = commands.Bot(command_prefix="!P")
 
 
 dump_file = 'point_totals.json' # TODO Change to just JSON file name because won't be in repo
+whitelist_file = 'whitelist.csv'
 
 
 @bot.command(name='award')
-async def award(ctx, *args):
-    keywords = args
-    username = keywords[0][3:-1]
-    points = int(keywords[1])
-    
+async def award(ctx, username, points):
+    with open('whitelist.csv', 'r') as f_wlist:
+        wlist = csv.reader(f_wlist)
+        if ctx.author not in wlist:
+            await ctx.send('Not Authorized to Use This Command')
+
+    username = username[3:-2] # Only keeps numbers
+    points = int(points)
     try:
         with open(dump_file, 'r') as f_read:
             try:
@@ -36,7 +40,8 @@ async def award(ctx, *args):
             data[username] = points
         f_write.seek(0)
         json.dump(data, f_write)
-    await ctx.send('Previous Points ({}) + New Points ({}) = Total Points ({})'.format(data[username] - points, points, data[username]))
+
+    await ctx.send('Previous Points ({}) + New Points ({}) = Total Points ({})'.format(int(data[username]) - points, points, data[username]))
 
 
 @bot.command('my_points')
